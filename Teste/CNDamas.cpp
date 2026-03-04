d351e2d1440ed958733e5e989e8c77926d643637
jcoelho72 <jcoelho@uab.pt>
2026-03-04 16:59:33 +0000

    código 10 resultado inválido

--- Teste/CNDamas.cpp
+++ Teste/CNDamas.cpp
@@ -49,7 +49,7 @@ int CNDamas::ExecutaAlgoritmo()
 		}
 		else {
 			printf("\nSolução inválida!");
-			MostrarSolucao();
+			indicators[IND_RESULTADO] = 10; // código para indicar que a solução é inválida
 		}
 	}
 
